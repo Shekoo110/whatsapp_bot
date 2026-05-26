b047a254acd7a62adf4acd0b31c5479947d98fd6
shekoo110 <alshekh989@gmail.com>
2026-05-27 02:51:25 +0300

    first upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+
+const fs = require("fs");
+const path = require("path");
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('البوت اشتغل');
+});
+
+client.on('message', async message => {
+
+    // ===== .صوره =====
+    if (message.body === '.صوره') {
+
+        const folder = './images';
+
+        const files = fs.readdirSync(folder);
+
+        const random =
+            files[Math.floor(Math.random() * files.length)];
+
+        const imagePath =
+            path.join(folder, random);
+
+        const media =
+            MessageMedia.fromFilePath(imagePath);
+
+        await client.sendMessage(
+            message.from,
+            media
+        );
+    }
+
+    // ===== .صوت =====
+    if (message.body === '.صوت') {
+
+        const folder = './audio';
+
+        const files = fs.readdirSync(folder);
+
+        const random =
+            files[Math.floor(Math.random() * files.length)];
+
+        const audioPath =
+            path.join(folder, random);
+
+        const media =
+            MessageMedia.fromFilePath(audioPath);
+
+        await client.sendMessage(
+            message.from,
+            media,
+            { sendAudioAsVoice: false }
+        );
+    }
+
+    // ===== .اصوات =====
+    if (message.body === '.اصوات') {
+
+        const folder = './sounds';
+
+        const files = fs.readdirSync(folder);
+
+        if (files.length === 0) {
+            await message.reply('لا توجد اصوات');
+            return;
+        }
+
+        for (const file of files) {
+
+            const audioPath =
+                path.join(folder, file);
+
+            const media =
+                MessageMedia.fromFilePath(audioPath);
+
+            await client.sendMessage(
+                message.from,
+                media,
+                { sendAudioAsVoice: false }
+            );
+        }
+    }
+
+});
+
+client.initialize();
