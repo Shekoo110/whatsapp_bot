8e96c1d2079b3e9d9c098ac5da4e9eb3ea3a645c
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-30 15:19:31 +0300

    Create Player.js

---
+++ models/Player.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const PlayerSchema = new mongoose.Schema({
+    userId: String,
+    pulls: { type: Number, default: 5 },
+    lastReset: { type: Number, default: Date.now },
+    characters: { type: Array, default: [] },
+    hp: { type: Number, default: 10000 },
+    crit: { type: Number, default: 5 },
+    dodge: { type: Number, default: 3 },
+    xp: { type: Number, default: 0 },
+    level: { type: Number, default: 1 },
+    money: { type: Number, default: 0 }
+})
+
+module.exports = mongoose.model('Player', PlayerSchema)
