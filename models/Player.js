669d1ac137d152864bec963d5ff69dbcf2b33454
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-31 02:03:17 +0300

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -4,13 +4,20 @@ const PlayerSchema = new mongoose.Schema({
     userId: String,
     pulls: { type: Number, default: 5 },
     lastReset: { type: Number, default: Date.now },
+
     characters: { type: Array, default: [] },
+
     hp: { type: Number, default: 10000 },
     crit: { type: Number, default: 5 },
     dodge: { type: Number, default: 3 },
+
     xp: { type: Number, default: 0 },
     level: { type: Number, default: 1 },
-    money: { type: Number, default: 0 }
+
+    money: { type: Number, default: 0 },
+
+    dailyBattles: { type: Number, default: 5 },
+    lastBattleReset: { type: Number, default: Date.now }
 })
 
 module.exports = mongoose.model('Player', PlayerSchema)
