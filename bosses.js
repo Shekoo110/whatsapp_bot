731067350d1f25f478eaea40df6ef715b83d8c82
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-31 15:37:58 +0300

    Create bosses.js

---
+++ bosses.js
@@ -0,0 +1,54 @@
+const bosses = [
+{
+    name: "Aizen",
+    hp: 50000,
+    maxHp: 50000
+},
+{
+    name: "Yhwach",
+    hp: 70000,
+    maxHp: 70000
+},
+{
+    name: "Madara",
+    hp: 65000,
+    maxHp: 65000
+},
+{
+    name: "Kaido",
+    hp: 80000,
+    maxHp: 80000
+},
+{
+    name: "Joy Boy",
+    hp: 90000,
+    maxHp: 90000
+},
+{
+    name: "Imu",
+    hp: 100000,
+    maxHp: 100000
+},
+{
+    name: "Tokinada",
+    hp: 60000,
+    maxHp: 60000
+},
+{
+    name: "Meruem",
+    hp: 55000,
+    maxHp: 55000
+},
+{
+    name: "Teach",
+    hp: 75000,
+    maxHp: 75000
+},
+{
+    name: "Roger",
+    hp: 85000,
+    maxHp: 85000
+}
+]
+
+module.exports = bosses
