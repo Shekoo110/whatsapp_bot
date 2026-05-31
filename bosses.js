0609c481e338d13ee60a3b07ef335b91211cee70
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-31 16:03:04 +0300

    Update bosses.js

--- bosses.js
+++ bosses.js
@@ -1,54 +1,125 @@
 const bosses = [
+
 {
     name: "Aizen",
     hp: 50000,
-    maxHp: 50000
+    maxHp: 50000,
+    image: "رابط",
+    ability: {
+        name: "Kyoka Suigetsu",
+        description: "قام أيزن بخداع الجميع وخفض ضرر الهجمات القادمة 50%",
+        effect: "halfDamage"
+    }
 },
+
 {
     name: "Yhwach",
     hp: 70000,
-    maxHp: 70000
+    maxHp: 70000,
+    image: "رابط",
+    ability: {
+        name: "The Almighty",
+        description: "رأى يوهاباخ المستقبل وتجنب الهجمة بالكامل",
+        effect: "dodge"
+    }
 },
+
 {
     name: "Madara",
     hp: 65000,
-    maxHp: 65000
+    maxHp: 65000,
+    image: "رابط",
+    ability: {
+        name: "Susanoo",
+        description: "استدعى السوسانو واستعاد 5000 HP",
+        effect: "heal"
+    }
 },
+
 {
     name: "Kaido",
     hp: 80000,
-    maxHp: 80000
+    maxHp: 80000,
+    image: "رابط",
+    ability: {
+        name: "Dragon Form",
+        description: "تحول إلى تنين وزاد دفاعه",
+        effect: "reduceDamage"
+    }
 },
+
 {
     name: "Joy Boy",
     hp: 90000,
-    maxHp: 90000
+    maxHp: 90000,
+    image: "رابط",
+    ability: {
+        name: "Nika",
+        description: "أطلق قوة نيكا وألحق ضرراً مضاعفاً",
+        effect: "doubleDamage"
+    }
 },
+
 {
     name: "Imu",
     hp: 100000,
-    maxHp: 100000
+    maxHp: 100000,
+    image: "رابط",
+    ability: {
+        name: "Unknown Power",
+        description: "استعاد 10000 HP من قوته الغامضة",
+        effect: "bigHeal"
+    }
 },
-{
-    name: "Tokinada",
-    hp: 60000,
-    maxHp: 60000
-},
+
 {
     name: "Meruem",
     hp: 55000,
-    maxHp: 55000
+    maxHp: 55000,
+    image: "رابط",
+    ability: {
+        name: "Evolution",
+        description: "تطور ميرويم واكتسب 3000 HP",
+        effect: "heal"
+    }
 },
+
 {
     name: "Teach",
     hp: 75000,
-    maxHp: 75000
+    maxHp: 75000,
+    image: "رابط",
+    ability: {
+        name: "Darkness",
+        description: "ابتلع الظلام جزءاً من الضرر",
+        effect: "halfDamage"
+    }
 },
+
 {
     name: "Roger",
     hp: 85000,
-    maxHp: 85000
+    maxHp: 85000,
+    image: "رابط",
+    ability: {
+        name: "Conqueror Haki",
+        description: "أطلق الهاكي الملكي وأضعف المهاجمين",
+        effect: "reduceDamage"
+    }
+},
+
+{
+    name: "Tokinada",
+    hp: 60000,
+    maxHp: 60000,
+    image: "رابط",
+    ability: {
+        name: "Enrakyoten",
+        description: "نسخ قدرة قوية واستعاد 4000 HP",
+        effect: "heal"
+    }
 }
+
 ]
 
 module.exports = bosses
