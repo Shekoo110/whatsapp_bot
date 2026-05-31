7d43c5ab21fbd2636ec733d2916aa4b6783c86ad
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-05-31 16:59:11 +0300

    Update bosses.js

--- bosses.js
+++ bosses.js
@@ -1,123 +1,123 @@
 const bosses = [
 
 {
-    name: "Aizen",
-    hp: 50000,
-    maxHp: 50000,
-    image: "رابط",
-    ability: {
-        name: "Kyoka Suigetsu",
-        description: "قام أيزن بخداع الجميع وخفض ضرر الهجمات القادمة 50%",
-        effect: "halfDamage"
-    }
+name: "Aizen",
+hp: 50000,
+maxHp: 50000,
+image: "https://i.ibb.co/9ksxPcRw/527968451e980d63fa2d4a7c1895b289.jpg",
+ability: {
+name: "Kyoka Suigetsu",
+description: "قام أيزن بخداع الجميع وخفض الضرر القادم 50%",
+effect: "halfDamage"
+}
 },
 
 {
-    name: "Yhwach",
-    hp: 70000,
-    maxHp: 70000,
-    image: "رابط",
-    ability: {
-        name: "The Almighty",
-        description: "رأى يوهاباخ المستقبل وتجنب الهجمة بالكامل",
-        effect: "dodge"
-    }
+name: "Yhwach",
+hp: 70000,
+maxHp: 70000,
+image: "https://i.ibb.co/HW8VPVx/52432833eade63aa7067622060e66183.jpg",
+ability: {
+name: "The Almighty",
+description: "رأى يوهاباخ المستقبل وتجنب الهجمة بالكامل",
+effect: "dodge"
+}
 },
 
 {
-    name: "Madara",
-    hp: 65000,
-    maxHp: 65000,
-    image: "رابط",
-    ability: {
-        name: "Susanoo",
-        description: "استدعى السوسانو واستعاد 5000 HP",
-        effect: "heal"
-    }
+name: "Tokinada",
+hp: 60000,
+maxHp: 60000,
+image: "https://i.ibb.co/LDdFC0sT/17145c15b01b425c4562f5dedda5ae28.jpg",
+ability: {
+name: "Enrakyoten",
+description: "نسخ قدرة قوية واستعاد 4000 HP",
+effect: "heal"
+}
 },
 
 {
-    name: "Kaido",
-    hp: 80000,
-    maxHp: 80000,
-    image: "رابط",
-    ability: {
-        name: "Dragon Form",
-        description: "تحول إلى تنين وزاد دفاعه",
-        effect: "reduceDamage"
-    }
+name: "Imu",
+hp: 100000,
+maxHp: 100000,
+image: "https://i.ibb.co/pvVx2yTN/24f563ae16e1fd49977688a5c800c7e6.jpg",
+ability: {
+name: "Unknown Power",
+description: "استعاد 10000 HP من قوته الغامضة",
+effect: "bigHeal"
+}
 },
 
 {
-    name: "Joy Boy",
-    hp: 90000,
-    maxHp: 90000,
-    image: "رابط",
-    ability: {
-        name: "Nika",
-        description: "أطلق قوة نيكا وألحق ضرراً مضاعفاً",
-        effect: "doubleDamage"
-    }
+name: "Joy Boy",
+hp: 90000,
+maxHp: 90000,
+image: "https://i.ibb.co/fGMK2s9z/c59baa23779c58bc8351e2f4a308d419.jpg",
+ability: {
+name: "Nika",
+description: "أطلق قوة نيكا وألحق ضرراً مضاعفاً",
+effect: "doubleDamage"
+}
 },
 
 {
-    name: "Imu",
-    hp: 100000,
-    maxHp: 100000,
-    image: "رابط",
-    ability: {
-        name: "Unknown Power",
-        description: "استعاد 10000 HP من قوته الغامضة",
-        effect: "bigHeal"
-    }
+name: "Madara",
+hp: 65000,
+maxHp: 65000,
+image: "https://i.ibb.co/whT9p1GX/6d2b34e75a541170762589b638248d60.jpg",
+ability: {
+name: "Susanoo",
+description: "استدعى السوسانو واستعاد 5000 HP",
+effect: "heal"
+}
 },
 
 {
-    name: "Meruem",
-    hp: 55000,
-    maxHp: 55000,
-    image: "رابط",
-    ability: {
-        name: "Evolution",
-        description: "تطور ميرويم واكتسب 3000 HP",
-        effect: "heal"
-    }
+name: "Kaido",
+hp: 80000,
+maxHp: 80000,
+image: "https://i.ibb.co/m531jPmT/3efd2af2492ded6dc0c1f69b1c65c533.jpg",
+ability: {
+name: "Dragon Form",
+description: "تحول إلى تنين وخفض الضرر المستلم",
+effect: "reduceDamage"
+}
 },
 
 {
-    name: "Teach",
-    hp: 75000,
-    maxHp: 75000,
-    image: "رابط",
-    ability: {
-        name: "Darkness",
-        description: "ابتلع الظلام جزءاً من الضرر",
-        effect: "halfDamage"
-    }
+name: "Roger",
+hp: 85000,
+maxHp: 85000,
+image: "https://i.ibb.co/rKwwTPhv/49eabbd5a9f5ee5fd47ed6eef7ac1156.jpg",
+ability: {
+name: "Conqueror Haki",
+description: "أطلق الهاكي الملكي وأضعف المهاجمين",
+effect: "reduceDamage"
+}
 },
 
 {
-    name: "Roger",
-    hp: 85000,
-    maxHp: 85000,
-    image: "رابط",
-    ability: {
-        name: "Conqueror Haki",
-        description: "أطلق الهاكي الملكي وأضعف المهاجمين",
-        effect: "reduceDamage"
-    }
+name: "Meruem",
+hp: 55000,
+maxHp: 55000,
+image: "https://i.ibb.co/M5Jkz69y/af5a90f6f02b1b6671b51a4c701a5ed6.jpg",
+ability: {
+name: "Evolution",
+description: "تطور ميرويم واستعاد 3000 HP",
+effect: "heal"
+}
 },
 
 {
-    name: "Tokinada",
-    hp: 60000,
-    maxHp: 60000,
-    image: "رابط",
-    ability: {
-        name: "Enrakyoten",
-        description: "نسخ قدرة قوية واستعاد 4000 HP",
-        effect: "heal"
-    }
+name: "Teach",
+hp: 75000,
+maxHp: 75000,
+image: "https://i.ibb.co/ns9FFjSG/4905692f38d6e5225eab3be16b5ab855.jpg",
+ability: {
+name: "Darkness",
+description: "ابتلع الظلام جزءاً من الضرر",
+effect: "halfDamage"
+}
 }
 
 ]
