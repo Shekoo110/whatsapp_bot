6b8feb437e8859e25c27ed11e1205246775d2a2b
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-02 04:51:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4013,66 +4013,65 @@ while (
     break
 }
 
-    levelUpMessage += `\n🎉 مبروك! وصلت إلى لفل ${me.level}\n`
+    
+levelUpMessage += `\n🎉 مبروك! وصلت إلى لفل ${me.level}\n`
 
-let ability = null;
+const ability = (me.level % 5 === 0)
+  ? levelAbilities?.[me.level]
+  : null;
 
-if (me.level % 5 === 0) {
-  ability = levelAbilities?.[me.level] || null;
+if (ability) {
 
-  if (ability) {
+  if (!me.specialAbilities)
+    me.specialAbilities = [];
 
-    if (!me.specialAbilities)
-      me.specialAbilities = [];
+  if (!me.specialAbilities.includes(ability.name)) {
 
-    if (!me.specialAbilities.includes(ability.name)) {
+    me.specialAbilities.push(ability.name);
 
-      me.specialAbilities.push(ability.name);
+    me.attackBonus = me.attackBonus || 0;
+    me.defenseBonus = me.defenseBonus || 0;
+    me.critBonus = me.critBonus || 0;
+    me.dodgeBonus = me.dodgeBonus || 0;
+    me.reflectBonus = me.reflectBonus || 0;
+    me.lifestealBonus = me.lifestealBonus || 0;
+    me.bossDamageBonus = me.bossDamageBonus || 0;
 
-      me.attackBonus = me.attackBonus || 0;
-      me.defenseBonus = me.defenseBonus || 0;
-      me.critBonus = me.critBonus || 0;
-      me.dodgeBonus = me.dodgeBonus || 0;
-      me.reflectBonus = me.reflectBonus || 0;
-      me.lifestealBonus = me.lifestealBonus || 0;
-      me.bossDamageBonus = me.bossDamageBonus || 0;
+    me.bonusAppliedLevels = me.bonusAppliedLevels || [];
 
-      // 🔥 منع تكرار البونس لنفس المستوى
-      me.bonusAppliedLevels = me.bonusAppliedLevels || [];
+    if (!me.bonusAppliedLevels.includes(me.level)) {
 
-      if (!me.bonusAppliedLevels.includes(me.level)) {
-
-        switch (me.level) {
-          case 5:  me.critBonus += 2; break;
-          case 10: me.attackBonus += 2; break;
-          case 15: me.defenseBonus += 2; break;
-          case 20: me.bossDamageBonus += 5; break;
-          case 25: me.dodgeBonus += 2; break;
-          case 30: me.attackBonus += 4; break;
-          case 35: me.critBonus += 3; break;
-          case 40: me.defenseBonus += 4; break;
-          case 45: me.reflectBonus += 3; break;
-          case 50: me.lifestealBonus += 5; break;
-          case 55: me.attackBonus += 5; break;
-          case 60: me.bossDamageBonus += 10; break;
-          case 65: me.dodgeBonus += 4; break;
-          case 70: me.critBonus += 5; break;
-          case 75: me.defenseBonus += 5; break;
-          case 80: me.attackBonus += 6; break;
-          case 85: me.reflectBonus += 5; break;
-          case 90: me.lifestealBonus += 8; break;
-          case 95: me.bossDamageBonus += 15; break;
-          case 100:
-            me.attackBonus += 10;
-            me.critBonus += 10;
-            me.defenseBonus += 10;
-            break;
-        }
-
-        me.bonusAppliedLevels.push(me.level);
+      switch (me.level) {
+        case 5:  me.critBonus += 2; break;
+        case 10: me.attackBonus += 2; break;
+        case 15: me.defenseBonus += 2; break;
+        case 20: me.bossDamageBonus += 5; break;
+        case 25: me.dodgeBonus += 2; break;
+        case 30: me.attackBonus += 4; break;
+        case 35: me.critBonus += 3; break;
+        case 40: me.defenseBonus += 4; break;
+        case 45: me.reflectBonus += 3; break;
+        case 50: me.lifestealBonus += 5; break;
+        case 55: me.attackBonus += 5; break;
+        case 60: me.bossDamageBonus += 10; break;
+        case 65: me.dodgeBonus += 4; break;
+        case 70: me.critBonus += 5; break;
+        case 75: me.defenseBonus += 5; break;
+        case 80: me.attackBonus += 6; break;
+        case 85: me.reflectBonus += 5; break;
+        case 90: me.lifestealBonus += 8; break;
+        case 95: me.bossDamageBonus += 15; break;
+        case 100:
+          me.attackBonus += 10;
+          me.critBonus += 10;
+          me.defenseBonus += 10;
+          break;
       }
 
-      levelUpMessage += `
+      me.bonusAppliedLevels.push(me.level);
+    }
+
+    levelUpMessage += `
 ✨ قدرة جديدة
 
 ${ability.name}
@@ -4082,7 +4081,6 @@ ${ability.name}
 🎯 نسبة التفعيل:
 ${ABILITY_CHANCE}% أثناء القتال
 `;
-    }
   }
 }
 
