ea175df76b46d7bc5283d3d421ba836399e3bec1
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-03 20:16:47 +0300

    Create PvP.js

---
+++ models/PvP.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const pvpSchema = new mongoose.Schema({
+
+    player1: {
+        type: String,
+        required: true
+    },
+
+    player2: {
+        type: String,
+        required: true
+    },
+
+    hp1: {
+        type: Number,
+        default: 10000
+    },
+
+    hp2: {
+        type: Number,
+        default: 10000
+    },
+
+    turn: {
+        type: String,
+        required: true
+    },
+
+    active: {
+        type: Boolean,
+        default: true
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('PvP', pvpSchema)
