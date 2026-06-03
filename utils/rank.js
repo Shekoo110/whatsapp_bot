adb23d757c7893739664aa9afda4b0edb70177a1
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-04 00:10:01 +0300

    Update getRank function for new rank tiers

--- utils/rank.js
+++ utils/rank.js
@@ -1,11 +1,21 @@
 function getRank(mmr) {
 
-    if (mmr < 1100) return "برونزي"
-    if (mmr < 1300) return "فضي"
-    if (mmr < 1500) return "ذهبي"
-    if (mmr < 1800) return "بلاتيني"
-    if (mmr < 2100) return "ماسي"
-    return "أسطوري"
+    if (mmr >= 3000)
+        return '👑 أسطوري'
+
+    if (mmr >= 2500)
+        return '💠 ماستر'
+
+    if (mmr >= 2000)
+        return '💎 ألماسي'
+
+    if (mmr >= 1500)
+        return '🥇 بلاتيني'
+
+    if (mmr >= 1200)
+        return '🥈 ذهبي'
+
+    return '🥉 برونزي'
 }
 
 module.exports = getRank
