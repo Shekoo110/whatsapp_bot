9a4c4c89abd20d4df3c3cf6c3357c0e5e47cd7da
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-04 00:09:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,24 +16,6 @@ console.log('Bot starting...')
 const mongoose = require('mongoose')
 const PvP = require('./models/PvP')
 
-function getRank(mmr) {
-
-    if (mmr >= 3000)
-        return '👑 أسطوري'
-
-    if (mmr >= 2500)
-        return '💠 ماستر'
-
-    if (mmr >= 2000)
-        return '💎 ألماسي'
-
-    if (mmr >= 1500)
-        return '🥇 بلاتيني'
-
-    if (mmr >= 1200)
-        return '🥈 ذهبي'
-
-    return '🥉 برونزي'
 }
 const bosses = require('./bosses')
 const characters = require('./characters.json')
