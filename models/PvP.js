86eda49183d7774be652390eae5220d49931837d
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-04 01:49:52 +0300

    Update PvP.js

--- models/PvP.js
+++ models/PvP.js
@@ -22,6 +22,48 @@ const pvpSchema = new mongoose.Schema({
         default: 10000
     },
 
+    shield1: {
+        type: Number,
+        default: 0
+    },
+
+    shield2: {
+        type: Number,
+        default: 0
+    },
+
+    team1: {
+        type: Array,
+        default: []
+    },
+
+    team2: {
+        type: Array,
+        default: []
+    },
+
+    burn: {
+        player1: {
+            type: Number,
+            default: 0
+        },
+        player2: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    poison: {
+        player1: {
+            type: Number,
+            default: 0
+        },
+        player2: {
+            type: Number,
+            default: 0
+        }
+    },
+
     turn: {
         type: String,
         required: true
