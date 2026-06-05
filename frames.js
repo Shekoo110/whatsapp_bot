f49bcc126917e09c55d0f19b167a9861b2864821
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-05 06:45:56 +0300

    Update frames.js

--- frames.js
+++ frames.js
@@ -1,6 +1,9 @@
 module.exports = {
-    R: "رابط إطار R",
-    SR: "رابط إطار SR",
-    SSR: "رابط إطار SSR",
-    UR: "رابط إطار UR"
+    R: "https://i.ibb.co/JRRBtSNn/file-00000000e6ac71fd992eef1f5c387d58.png",
+
+    SR: "https://i.ibb.co/d4thBsdp/file-000000000d08723089c342617d09c602.png",
+
+    SSR: "https://i.ibb.co/Cs9mw2cY/file-0000000073b871f891a1c5a506f34215.png",
+
+    UR: "https://i.ibb.co/Fbm332Wr/file-00000000c4c871fd9033548dc2aa0b82.png"
 }
