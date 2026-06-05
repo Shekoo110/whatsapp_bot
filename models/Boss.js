14f65e1a9b9730ef0eeb51e455715297857625cb
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-05 03:30:59 +0300

    Update Boss.js

--- models/Boss.js
+++ models/Boss.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose')
 
 const BossSchema = new mongoose.Schema({
+
     name: String,
     image: String,
+
     hp: Number,
     maxHp: Number,
-    ability: Object
+    attack: Number,
+
+    ability: Object,
+    abilities: Array,
+
+    followers: Array,
+    activeFollowers: Array,
+
+    enraged: Boolean,
+    turnCounter: Number,
+    groupAttackCount: Number,
+
+    killer: String,
+    finished: Boolean
+
 })
 
-module.exports = mongoose.model('Boss', BossSchema)
+module.exports =
+    mongoose.model('Boss', BossSchema)
