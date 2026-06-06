b832e28b9dd6ab967794818fedee5b91fec93b62
Shekoo110 <150535160+Shekoo110@users.noreply.github.com>
2026-06-06 18:30:26 +0300

    Refactor waifuSchema with unique anilistId
    Updated waifuSchema to include unique anilistId and modified default values for rarity and value fields.

--- models/Waifu.js
+++ models/Waifu.js
@@ -1,26 +1,62 @@
 const mongoose = require('mongoose')
 
 const waifuSchema = new mongoose.Schema({
-    anilistId: Number,
+
+    anilistId: {
+        type: Number,
+        unique: true
+    },
+
     name: String,
-    image: String,
+
     anime: String,
-    game: String,
+
+    image: String,
+
+    gender: {
+        type: String,
+        default: 'Female'
+    },
+
     rarity: {
         type: String,
-        default: 'SSR'
+        default: 'B'
     },
+
+    value: {
+        type: Number,
+        default: 100
+    },
+
+    claims: {
+        type: Number,
+        default: 0
+    },
+
+    likes: {
+        type: Number,
+        default: 0
+    },
+
     claimedBy: {
         type: String,
         default: null
     },
-    value: {
-        type: Number,
-        default: 1000
+
+    claimedAt: {
+        type: Date,
+        default: null
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
+
 })
 
-module.exports = mongoose.model(
+module.exports =
+mongoose.model(
     'Waifu',
     waifuSchema
 )
