d2dc1c0b13f21540c31aa0a2e4d23d5cd52fbb96
Shekoo110 <alshekh989@gmail.com>
2026-06-07 10:24:24 +0300

    Update importWaifus.js

--- importWaifus.js
+++ importWaifus.js
@@ -1,83 +1,115 @@
 const axios = require('axios')
 const Waifu = require('./models/Waifu')
-const cheerio = require("cheerio"); // إذا غير موجود أضفه
-
-const animeWikiMap = {
-    "BLEACH": "https://bleach.fandom.com/wiki",
-    "Naruto": "https://naruto.fandom.com/wiki",
-    "One Piece": "https://onepiece.fandom.com/wiki",
-    "Dragon Ball Z": "https://dragonball.fandom.com/wiki",
-    "Dragon Ball Super": "https://dragonball.fandom.com/wiki",
-    "Hunter x Hunter": "https://hunterxhunter.fandom.com/wiki",
-    "Fairy Tail": "https://fairytail.fandom.com/wiki",
-    "Jujutsu Kaisen": "https://jujutsu-kaisen.fandom.com/wiki",
-    "Black Clover": "https://blackclover.fandom.com/wiki",
-    "Demon Slayer": "https://kimetsu-no-yaiba.fandom.com/wiki",
-    "Attack on Titan": "https://attackontitan.fandom.com/wiki",
-    "Tokyo Ghoul": "https://tokyoghoul.fandom.com/wiki"
-};
-
-async function getWikiInfoboxImage(name, wikiBaseUrl) {
-    try {
-        const url = `${wikiBaseUrl}/${name.replace(/ /g, "_")}`;
-
-        const { data } = await axios.get(url);
-
-        const $ = cheerio.load(data);
-
-        const img =
-            $(".pi-image-thumbnail").attr("src") ||
-            $(".image img").first().attr("src");
-
-        return img || null;
-
-    } catch (err) {
-        return null;
-    }
-}
 
 function sleep(ms) {
-    return new Promise(
-        resolve =>
-            setTimeout(resolve, ms)
-    )
+return new Promise(
+resolve =>
+setTimeout(resolve, ms)
+)
 }
 
 const animes = [
-    'BLEACH',
-    'Naruto',
-    'One Piece',
-    'Dragon Ball Z',
-    'Dragon Ball Super',
-    'Hunter x Hunter',
-    'Fairy Tail',
-    'Jujutsu Kaisen',
-    'Black Clover',
-    'Demon Slayer',
-    'Attack on Titan',
-    'Tokyo Ghoul'
+'BLEACH',
+'Naruto',
+'One Piece',
+'Dragon Ball Z',
+'Dragon Ball Super',
+'Hunter x Hunter',
+'Fairy Tail',
+'Jujutsu Kaisen',
+'Black Clover',
+'Demon Slayer',
+'Attack on Titan',
+'Tokyo Ghoul',
+
+'Oshi no Ko',
+
+'Fate/stay night',
+'Fate/Zero',
+'Fate/Apocrypha',
+'Fate/Grand Order'
+
 ]
 
 function getRarity() {
 
-    const r = Math.random()
+const r = Math.random()
 
-    if (r < 0.01) return 'SSS'
-    if (r < 0.05) return 'SS'
-    if (r < 0.15) return 'S'
-    if (r < 0.35) return 'A'
+if (r < 0.01) return 'SSS'
+if (r < 0.05) return 'SS'
+if (r < 0.15) return 'S'
+if (r < 0.35) return 'A'
 
-    return 'B'
+return 'B'
+
 }
 
 async function searchAnime(title) {
 
+const query = `
+query ($search:String) {
+    Media(search:$search,type:ANIME) {
+        id
+        title {
+            romaji
+        }
+    }
+}`
+
+const res = await axios.post(
+    'https://graphql.anilist.co',
+    {
+        query,
+        variables: {
+            search: title
+        }
+    }
+)
+
+return res.data.data.Media
+
+}
+
+async function getCharacters(id) {
+
+let allCharacters = []
+let page = 1
+let hasNextPage = true
+let animeTitle = ''
+
+while (hasNextPage) {
+
     const query = `
-    query ($search:String) {
-        Media(search:$search,type:ANIME) {
-            id
+    query ($id:Int,$page:Int) {
+        Media(id:$id,type:ANIME) {
+
             title {
                 romaji
+            }
+
+            characters(
+                page:$page
+                perPage:50
+            ) {
+
+                pageInfo {
+                    hasNextPage
+                }
+
+                nodes {
+
+                    id
+                    gender
+                    favourites
+
+                    name {
+                        full
+                    }
+
+                    image {
+                        large
+                    }
+                }
             }
         }
     }`
@@ -87,115 +119,49 @@ async function searchAnime(title) {
         {
             query,
             variables: {
-                search: title
+                id,
+                page
             }
         }
     )
 
-    return res.data.data.Media
+    await sleep(1500)
+
+    const media =
+        res.data.data.Media
+
+    animeTitle =
+        media.title.romaji
+
+    allCharacters.push(
+        ...media.characters.nodes
+    )
+
+    hasNextPage =
+        media.characters.pageInfo.hasNextPage
+
+    page++
 }
 
-async function getCharacters(id) {
-
-    let allCharacters = []
-    let page = 1
-    let hasNextPage = true
-    let animeTitle = ''
-
-    while (hasNextPage) {
-
-        const query = `
-        query ($id:Int,$page:Int) {
-            Media(id:$id,type:ANIME) {
-
-                title {
-                    romaji
-                }
-
-                characters(
-                    page:$page
-                    perPage:50
-                ) {
-
-                    pageInfo {
-                        hasNextPage
-                    }
-
-                    nodes {
-
-                        id
-                        gender
-                        favourites
-
-                        name {
-                            full
-                        }
-
-                        image {
-                            large
-                        }
-                    }
-                }
-            }
-        }`
-
-        const res = await axios.post(
-    'https://graphql.anilist.co',
-    {
-        query,
-        variables: {
-            id,
-            page
-        }
+return {
+    title: {
+        romaji: animeTitle
+    },
+    characters: {
+        nodes: allCharacters
     }
-)
+}
 
-await sleep(1500)
-
-        const media =
-            res.data.data.Media
-
-        animeTitle =
-            media.title.romaji
-
-        allCharacters.push(
-            ...media.characters.nodes
-        )
-
-        hasNextPage =
-            media.characters.pageInfo.hasNextPage
-
-        page++
-    }
-
-    return {
-        title: {
-            romaji: animeTitle
-        },
-        characters: {
-            nodes: allCharacters
-        }
-    }
 }
 
 module.exports = async function importWaifus() {
 
-    const count =
-        await Waifu.countDocuments()
+let imported = 0
 
-    if (count > 0) {
+for (const anime of animes) {
 
-        console.log(
-            'Anime waifus already imported'
-        )
+    await sleep(3000)
 
-        return
-    }
-
-    let imported = 0
-
-    for (const anime of animes) {
-await sleep(3000)
     try {
 
         console.log(
@@ -236,23 +202,26 @@ await sleep(3000)
             if (exists)
                 continue
 
-            const baseUrl = animeWikiMap[data.title.romaji];
+            await Waifu.create({
+                anilistId: c.id,
+                name: c.name.full,
+                anime: data.title.romaji,
 
-const wikiImage = baseUrl
-    ? await getWikiInfoboxImage(c.name.full, baseUrl)
-    : null;
+                image: c.image.large,
 
-await Waifu.create({
-    anilistId: c.id,
-    name: c.name.full,
-    anime: data.title.romaji,
+                imageSource: 'anilist',
+                imageUpdated: true,
+                imageUpdatedAt: new Date(),
 
-    image: wikiImage || c.image.large,
-
-    gender: 'Female',
-    rarity: getRarity(),
-    value: Math.max(100, Math.floor((c.favourites || 0) / 10))
-});
+                gender: 'Female',
+                rarity: getRarity(),
+                value: Math.max(
+                    100,
+                    Math.floor(
+                        (c.favourites || 0) / 10
+                    )
+                )
+            })
 
             imported++
         }
@@ -268,7 +237,8 @@ await Waifu.create({
         )
 
         console.log(
-            err.response?.data || err.message
+            err.response?.data ||
+            err.message
         )
     }
 }
