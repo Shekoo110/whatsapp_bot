db4fe23cc75b17c325093867a883889d73a903fd
Shekoo110 <alshekh989@gmail.com>
2026-06-07 09:43:36 +0300

    Improve image extraction in getBleachImage.js
    Refactor image extraction logic to prioritize Open Graph image.

--- getBleachImage.js
+++ getBleachImage.js
@@ -23,7 +23,19 @@ module.exports = async function getBleachImage(name) {
 
         let image = null
 
-        $('aside img').each((i, el) => {
+        image =
+            $('meta[property="og:image"]')
+                .attr('content')
+
+        if (
+            image &&
+            image.startsWith('http')
+        ) {
+
+            return image
+        }
+
+        $('img').each((i, el) => {
 
             const src =
                 $(el).attr('src')
@@ -46,6 +58,8 @@ module.exports = async function getBleachImage(name) {
         console.log(
             `BLEACH IMAGE ERROR: ${name}`
         )
+
+        console.log(err.message)
 
         return null
     }
