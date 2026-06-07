100e2a1e8d6f8ea765d84f88e5bd317344c025c7
Shekoo110 <alshekh989@gmail.com>
2026-06-07 05:37:18 +0300

    Update updateAnimeImages.js

--- updateAnimeImages.js
+++ updateAnimeImages.js
@@ -3,43 +3,58 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 const animeWikiMap = {
-"BLEACH": "https://bleach.fandom.com/wiki",
-"Naruto": "https://naruto.fandom.com/wiki",
-"One Piece": "https://onepiece.fandom.com/wiki",
-"Dragon Ball Z": "https://dragonball.fandom.com/wiki",
-"Dragon Ball Super": "https://dragonball.fandom.com/wiki",
-"Hunter x Hunter": "https://hunterxhunter.fandom.com/wiki",
-"Fairy Tail": "https://fairytail.fandom.com/wiki",
-"Jujutsu Kaisen": "https://jujutsu-kaisen.fandom.com/wiki",
-"Black Clover": "https://blackclover.fandom.com/wiki",
-"Demon Slayer": "https://kimetsu-no-yaiba.fandom.com/wiki",
-"Attack on Titan": "https://attackontitan.fandom.com/wiki",
-"Tokyo Ghoul": "https://tokyoghoul.fandom.com/wiki"
+    "BLEACH": "https://bleach.fandom.com/wiki",
+
+    "Naruto": "https://naruto.fandom.com/wiki",
+
+    "ONE PIECE": "https://onepiece.fandom.com/wiki",
+    "One Piece": "https://onepiece.fandom.com/wiki",
+
+    "Dragon Ball Z": "https://dragonball.fandom.com/wiki",
+    "Dragon Ball Super": "https://dragonball.fandom.com/wiki",
+
+    "Hunter x Hunter": "https://hunterxhunter.fandom.com/wiki",
+
+    "Fairy Tail": "https://fairytail.fandom.com/wiki",
+
+    "Jujutsu Kaisen": "https://jujutsu-kaisen.fandom.com/wiki",
+
+    "Black Clover": "https://blackclover.fandom.com/wiki",
+
+    "Demon Slayer": "https://kimetsu-no-yaiba.fandom.com/wiki",
+    "Kimetsu no Yaiba": "https://kimetsu-no-yaiba.fandom.com/wiki",
+
+    "Attack on Titan": "https://attackontitan.fandom.com/wiki",
+    "Shingeki no Kyojin": "https://attackontitan.fandom.com/wiki",
+
+    "Tokyo Ghoul": "https://tokyoghoul.fandom.com/wiki",
+
+    "Onigiri": "https://onigiri.fandom.com/wiki"
 }
 
 async function getWikiImage(name, baseUrl) {
 
-try {
+    try {
 
-    const url =
-        `${baseUrl}/${name.replace(/ /g, "_")}`
+        const url =
+            `${baseUrl}/${name.replace(/ /g, "_")}`
 
-    const { data } =
-        await axios.get(url)
+        const { data } =
+            await axios.get(url)
 
-    const $ = cheerio.load(data)
+        const $ = cheerio.load(data)
 
-    const img =
-        $(".pi-image-thumbnail").attr("src") ||
-        $(".image img").first().attr("src")
+        const img =
+            $('meta[property="og:image"]').attr("content") ||
+            $(".pi-image-thumbnail").attr("src") ||
+            $(".image img").first().attr("src")
 
-    return img || null
+        return img || null
 
-} catch {
+    } catch {
 
-    return null
-}
-
+        return null
+    }
 }
 
 module.exports = async function updateAnimeImages(limit = 319) {
