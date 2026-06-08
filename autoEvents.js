e5e40704558bba3ffbedea358aeedfd12ed62970
Shekoo110 <alshekh989@gmail.com>
2026-06-08 18:09:43 +0300

    Create autoEvents.js

---
+++ autoEvents.js
@@ -0,0 +1,44 @@
+const eventManager =
+require('./eventManager')
+
+const eventGroups = [
+
+'120363020823525909@g.us',
+
+'120363400448225715@g.us',
+
+'120363362807326585@g.us'
+
+]
+
+function startAutoEvents(sock) {
+
+setInterval(
+    async () => {
+
+        try {
+
+            for (const groupId of eventGroups) {
+
+                await eventManager.startEvent(
+                    sock,
+                    groupId
+                )
+            }
+
+        } catch (err) {
+
+            console.log(
+                'Auto Event Error:',
+                err
+            )
+        }
+
+    },
+    20 * 60 * 1000
+)
+
+}
+
+module.exports =
+startAutoEvents
