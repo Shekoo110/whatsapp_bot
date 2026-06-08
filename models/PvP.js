12ad810b32129e3a6d99c8971d95f84c04590b92
Shekoo110 <alshekh989@gmail.com>
2026-06-08 23:27:10 +0300

    Refactor PvP schema for improved structure

--- models/PvP.js
+++ models/PvP.js
@@ -2,110 +2,115 @@ const mongoose = require('mongoose')
 
 const pvpSchema = new mongoose.Schema({
 
-player1: {
-    type: String,
-    required: true
-},
-
-player2: {
-    type: String,
-    required: true
-},
-
-hp1: {
-    type: Number,
-    default: 10000
-},
-
-hp2: {
-    type: Number,
-    default: 10000
-},
-
-shield1: {
-    type: Number,
-    default: 0
-},
-
-shield2: {
-    type: Number,
-    default: 0
-},
-
-team1: {
-    type: Array,
-    default: []
-},
-
-team2: {
-    type: Array,
-    default: []
-},
-
-burn: {
     player1: {
+        type: String,
+        required: true
+    },
+
+    player2: {
+        type: String,
+        required: true
+    },
+
+    hp1: {
+        type: Number,
+        default: 10000
+    },
+
+    hp2: {
+        type: Number,
+        default: 10000
+    },
+
+    shield1: {
         type: Number,
         default: 0
     },
-    player2: {
-        type: Number,
-        default: 0
-    }
-},
 
-poison: {
-    player1: {
+    shield2: {
         type: Number,
         default: 0
     },
-    player2: {
+
+    team1: {
+        type: Array,
+        default: []
+    },
+
+    team2: {
+        type: Array,
+        default: []
+    },
+
+    burn: {
+        player1: {
+            type: Number,
+            default: 0
+        },
+        player2: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    poison: {
+        player1: {
+            type: Number,
+            default: 0
+        },
+        player2: {
+            type: Number,
+            default: 0
+        }
+    },
+
+    skillTurn1: {
         type: Number,
         default: 0
+    },
+
+    skillTurn2: {
+        type: Number,
+        default: 0
+    },
+
+    ultimateTurn1: {
+        type: Number,
+        default: 0
+    },
+
+    ultimateTurn2: {
+        type: Number,
+        default: 0
+    },
+
+    turnCount: {
+        type: Number,
+        default: 1
+    },
+
+    turn: {
+        type: String,
+        required: true
+    },
+
+    active: {
+        type: Boolean,
+        default: true
+    },
+
+    lastMove: {
+        type: Date,
+        default: Date.now
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-},
-
-skillCooldown1: {
-    type: Number,
-    default: 0
-},
-
-skillCooldown2: {
-    type: Number,
-    default: 0
-},
-
-ultimateCooldown1: {
-    type: Number,
-    default: 0
-},
-
-ultimateCooldown2: {
-    type: Number,
-    default: 0
-},
-
-turn: {
-    type: String,
-    required: true
-},
-
-active: {
-    type: Boolean,
-    default: true
-},
-
-lastMove: {
-    type: Date,
-    default: Date.now
-},
-
-createdAt: {
-    type: Date,
-    default: Date.now
-}
 
 })
 
 module.exports =
-mongoose.models.PvP ||
-mongoose.model('PvP', pvpSchema)
+    mongoose.models.PvP ||
+    mongoose.model('PvP', pvpSchema)
