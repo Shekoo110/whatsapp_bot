5f6aefd0bd2bab759759c7e453a8d44c70a655ca
Shekoo110 <alshekh989@gmail.com>
2026-06-08 17:54:10 +0300

    Add event list with various commands

---
+++ eventList.js
@@ -0,0 +1,105 @@
+const events = [
+
+{
+    name: 'لف العجلة',
+    command: 'لف'
+},
+
+{
+    name: 'كرة قدم',
+    command: 'شوت'
+},
+
+{
+    name: 'كرة سلة',
+    command: 'رمي'
+},
+
+{
+    name: 'الصيد',
+    command: 'سنارة'
+},
+
+{
+    name: 'صندوق كنز',
+    command: 'فتح'
+},
+
+{
+    name: 'شخصية ظهرت',
+    command: 'تجنيد'
+},
+
+{
+    name: 'هدية طائرة',
+    command: 'التقاط'
+},
+
+{
+    name: 'مطر ذهب',
+    command: 'جمع'
+},
+
+{
+    name: 'وحش صغير',
+    command: '.اهجم'
+},
+
+{
+    name: 'تنين هائج',
+    command: '.اقتل'
+},
+
+{
+    name: 'نينجا هارب',
+    command: 'مطاردة'
+},
+
+{
+    name: 'حجر النرد',
+    command: 'رمي'
+},
+
+{
+    name: 'منجم',
+    command: 'تنقيب'
+},
+
+{
+    name: 'فاكهة شيطان',
+    command: 'اخذ'
+},
+
+{
+    name: 'سباق سريع',
+    command: 'جري'
+},
+
+{
+    name: 'باب غامض',
+    command: 'دخول'
+},
+
+{
+    name: 'مصباح سحري',
+    command: 'فرك'
+},
+
+{
+    name: 'بالون مكافآت',
+    command: 'فرقع'
+},
+
+{
+    name: 'رماية',
+    command: 'اطلاق'
+},
+
+{
+    name: 'جزيرة جديدة',
+    command: 'استكشاف'
+}
+
+]
+
+module.exports = events
