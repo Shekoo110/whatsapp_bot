b7eea694338e6af22b18c57ce9674f6d9a37d3e6
Shekoo110 <alshekh989@gmail.com>
2026-06-08 05:07:23 +0300

    Add getRandomQuestion function to quiz.js

--- quiz.js
+++ quiz.js
@@ -9,3 +9,60 @@ let scoreboard = {}
 let answeredUsers = new Set()
 
 let usedQuestions = []
+
+function getRandomQuestion() {
+
+    const availableQuestions =
+        questions.filter(
+            (_, index) =>
+                !usedQuestions.includes(index)
+        )
+
+    if (!availableQuestions.length) {
+
+        usedQuestions = []
+
+        return getRandomQuestion()
+    }
+
+    const randomQuestion =
+        availableQuestions[
+            Math.floor(
+                Math.random() *
+                availableQuestions.length
+            )
+        ]
+
+    const originalIndex =
+        questions.indexOf(randomQuestion)
+
+    usedQuestions.push(originalIndex)
+
+    return randomQuestion
+}
+
+module.exports = {
+    getRandomQuestion,
+
+    quizData: {
+        get quizActive() {
+            return quizActive
+        },
+        set quizActive(value) {
+            quizActive = value
+        },
+
+        get currentQuestion() {
+            return currentQuestion
+        },
+        set currentQuestion(value) {
+            currentQuestion = value
+        },
+
+        scoreboard,
+
+        answeredUsers,
+
+        usedQuestions
+    }
+}
