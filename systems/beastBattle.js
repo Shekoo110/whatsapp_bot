232e37e06e48b53d150581bc7c0bc5a899a43949
Shekoo110 <alshekh989@gmail.com>
2026-06-10 12:46:54 +0300

    Create beastBattle.js

---
+++ systems/beastBattle.js
@@ -0,0 +1,165 @@
+const beasts = [
+
+{
+    id: 'shukaku',
+    name: 'شكاكو',
+    rarity: 'rare',
+
+    attack: 0,
+    defense: 15,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 10,
+    skill: 'sand_shield'
+},
+
+{
+    id: 'matatabi',
+    name: 'ماتاتابي',
+    rarity: 'rare',
+
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 10,
+    reflect: 0,
+
+    skillChance: 10,
+    skill: 'blue_flame'
+},
+
+{
+    id: 'isobu',
+    name: 'إيسوبو',
+    rarity: 'rare',
+
+    attack: 0,
+    defense: 0,
+    hp: 10,
+    crit: 0,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 10,
+    skill: 'water_barrier'
+},
+
+{
+    id: 'son_goku',
+    name: 'سون غوكو',
+    rarity: 'legendary',
+
+    attack: 25,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 12,
+    skill: 'lava_punch'
+},
+
+{
+    id: 'kokuo',
+    name: 'كوكو',
+    rarity: 'legendary',
+
+    attack: 0,
+    defense: 0,
+    hp: 20,
+    crit: 0,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 12,
+    skill: 'steam_charge'
+},
+
+{
+    id: 'saiken',
+    name: 'سايكن',
+    rarity: 'legendary',
+
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 10,
+
+    skillChance: 12,
+    skill: 'acid_slime'
+},
+
+{
+    id: 'chomei',
+    name: 'تشومي',
+    rarity: 'legendary',
+
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 20,
+    reflect: 0,
+
+    skillChance: 12,
+    skill: 'flight_speed'
+},
+
+{
+    id: 'gyuki',
+    name: 'غيوكي',
+    rarity: 'epic',
+
+    attack: 35,
+    defense: 0,
+    hp: 25,
+    crit: 0,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 15,
+    skill: 'tentacle_crush'
+},
+
+{
+    id: 'kurama',
+    name: 'كوراما',
+    rarity: 'epic',
+
+    attack: 40,
+    defense: 0,
+    hp: 0,
+    crit: 20,
+    dodge: 0,
+    reflect: 0,
+
+    skillChance: 15,
+    skill: 'tailed_beast_bomb'
+},
+
+{
+    id: 'juubi',
+    name: 'الجوبي',
+    rarity: 'cosmic',
+
+    attack: 30,
+    defense: 30,
+    hp: 30,
+    crit: 30,
+    dodge: 30,
+    reflect: 30,
+
+    skillChance: 20,
+    skill: 'world_destruction'
+}
+
+]
+
+module.exports = beasts
