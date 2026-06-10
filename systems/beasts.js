6e5cee2da8a232a660946dbea1fd1c7033047894
Shekoo110 <alshekh989@gmail.com>
2026-06-10 12:58:21 +0300

    Create beasts.js

---
+++ systems/beasts.js
@@ -0,0 +1,114 @@
+const beasts = [
+{
+    id: 'shukaku',
+    name: 'شكاكو',
+    rarity: 'rare',
+    attack: 0,
+    defense: 15,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'matatabi',
+    name: 'ماتاتابي',
+    rarity: 'rare',
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 10,
+    reflect: 0
+},
+{
+    id: 'isobu',
+    name: 'إيسوبو',
+    rarity: 'rare',
+    attack: 0,
+    defense: 0,
+    hp: 10,
+    crit: 0,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'son_goku',
+    name: 'سون غوكو',
+    rarity: 'legendary',
+    attack: 25,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'kokuo',
+    name: 'كوكو',
+    rarity: 'legendary',
+    attack: 0,
+    defense: 0,
+    hp: 20,
+    crit: 0,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'saiken',
+    name: 'سايكن',
+    rarity: 'legendary',
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 10
+},
+{
+    id: 'chomei',
+    name: 'تشومي',
+    rarity: 'legendary',
+    attack: 0,
+    defense: 0,
+    hp: 0,
+    crit: 0,
+    dodge: 20,
+    reflect: 0
+},
+{
+    id: 'gyuki',
+    name: 'غيوكي',
+    rarity: 'epic',
+    attack: 35,
+    hp: 25,
+    defense: 0,
+    crit: 0,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'kurama',
+    name: 'كوراما',
+    rarity: 'epic',
+    attack: 40,
+    hp: 0,
+    defense: 0,
+    crit: 20,
+    dodge: 0,
+    reflect: 0
+},
+{
+    id: 'juubi',
+    name: 'الجوبي',
+    rarity: 'cosmic',
+    attack: 30,
+    defense: 30,
+    hp: 30,
+    crit: 30,
+    dodge: 30,
+    reflect: 30
+}
+]
+
+module.exports = beasts
