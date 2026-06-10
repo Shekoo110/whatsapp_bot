a63bb5ae0ccc9f89c7095e04b33078a7ba9ef368
Shekoo110 <alshekh989@gmail.com>
2026-06-10 12:48:26 +0300

    Add beastEggs data structure with beast chances

---
+++ data/beastEggs.js
@@ -0,0 +1,55 @@
+const beastEggs = [
+
+{
+    beast: 'شكاكو',
+    chance: 20
+},
+
+{
+    beast: 'ماتاتابي',
+    chance: 18
+},
+
+{
+    beast: 'إيسوبو',
+    chance: 15
+},
+
+{
+    beast: 'سون غوكو',
+    chance: 12
+},
+
+{
+    beast: 'كوكو',
+    chance: 10
+},
+
+{
+    beast: 'سايكن',
+    chance: 8
+},
+
+{
+    beast: 'تشومي',
+    chance: 7
+},
+
+{
+    beast: 'غيوكي',
+    chance: 5
+},
+
+{
+    beast: 'كوراما',
+    chance: 4
+},
+
+{
+    beast: 'الجوبي',
+    chance: 1
+}
+
+]
+
+module.exports = beastEggs
