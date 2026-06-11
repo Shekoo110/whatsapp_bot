39f2ec7888db71d03fcca2d4edf6fadb80df899f
Shekoo110 <alshekh989@gmail.com>
2026-06-11 18:49:32 +0300

    Create urAbilities.js

---
+++ urAbilities.js
@@ -0,0 +1,99 @@
+module.exports = [
+
+{
+name: "👹 قاتل الوحوش",
+type: "bossDamage",
+value: 20,
+description: "+20% ضرر ضد الزعماء",
+chance: 15
+},
+
+{
+name: "⚔️ سيد القتال",
+type: "attack",
+value: 15,
+description: "+15% هجوم",
+chance: 15
+},
+
+{
+name: "💀 ملك الدماء",
+type: "lifesteal",
+value: 10,
+description: "+10% امتصاص حياة",
+chance: 10
+},
+
+{
+name: "🛡️ ملك الملوك",
+type: "defense",
+value: 15,
+description: "+15% دفاع",
+chance: 10
+},
+
+{
+name: "👻 شبح الظلال",
+type: "dodge",
+value: 10,
+description: "+10% مراوغة",
+chance: 10
+},
+
+{
+name: "🎯 عين الصياد",
+type: "critRate",
+value: 10,
+description: "+10% ضربة حرجة",
+chance: 10
+},
+
+{
+name: "🪞 مرآة الانتقام",
+type: "reflect",
+value: 10,
+description: "+10% عكس ضرر",
+chance: 10
+},
+
+{
+name: "🛡️ درع الطاقة",
+type: "shield",
+value: 20,
+description: "+20% درع",
+chance: 10
+},
+
+{
+name: "☄️ مدمر الأكوان",
+type: "bossDamage",
+value: 30,
+description: "+30% ضرر ضد الزعماء",
+chance: 4
+},
+
+{
+name: "💥 محطم الجبال",
+type: "attack",
+value: 25,
+description: "+25% هجوم",
+chance: 3
+},
+
+{
+name: "🏰 حصن الأساطير",
+type: "defense",
+value: 25,
+description: "+25% دفاع",
+chance: 2
+},
+
+{
+name: "🌌 سيد الأكوان",
+type: "bossDamage",
+value: 50,
+description: "+50% ضرر ضد الزعماء",
+chance: 1
+}
+
+]
