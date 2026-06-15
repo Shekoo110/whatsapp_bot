26f9d50e7d8683ddca97c0188d0d61813c94d232
Shekoo110 <alshekh989@gmail.com>
2026-06-15 08:35:39 +0300

    Update pairing code in index.js

--- index.js
+++ index.js
@@ -1948,7 +1948,7 @@ if (!state.creds.registered) {
 
             const code =
                 await sock.requestPairingCode(
-                    "966562875546"
+                    "66937912691"
                 )
 
             console.log(
