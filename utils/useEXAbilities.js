4b8121fc5812db621e59a0d67640fa945a73e316
Shekoo110 <alshekh989@gmail.com>
2026-06-29 03:04:19 +0300

    Implement useEXAbilities function for character abilities

---
+++ utils/useEXAbilities.js
@@ -0,0 +1,81 @@
+function shuffle(array) {
+
+    return [...array].sort(() => Math.random() - 0.5)
+
+}
+
+function useEXAbilities(character) {
+
+    const abilities =
+        character.urAbilities || []
+
+    const selected =
+        shuffle(abilities).slice(0, 4)
+
+    const result = {
+
+        abilitiesUsed: [],
+
+        attackBonus: 0,
+        defenseBonus: 0,
+        bossDamage: 0,
+        critRate: 0,
+        critDamage: 0,
+        dodge: 0,
+        shield: 0,
+        lifesteal: 0,
+        reflect: 0
+
+    }
+
+    for (const ability of selected) {
+
+        result.abilitiesUsed.push(ability)
+
+        switch (ability.type) {
+
+            case 'attack':
+                result.attackBonus += ability.value
+                break
+
+            case 'defense':
+                result.defenseBonus += ability.value
+                break
+
+            case 'bossDamage':
+                result.bossDamage += ability.value
+                break
+
+            case 'critRate':
+                result.critRate += ability.value
+                break
+
+            case 'critDamage':
+                result.critDamage += ability.value
+                break
+
+            case 'dodge':
+                result.dodge += ability.value
+                break
+
+            case 'shield':
+                result.shield += ability.value
+                break
+
+            case 'lifesteal':
+                result.lifesteal += ability.value
+                break
+
+            case 'reflect':
+                result.reflect += ability.value
+                break
+
+        }
+
+    }
+
+    return result
+
+}
+
+module.exports = useEXAbilities
