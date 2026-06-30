f374268028a3702ae52f3ef1b61dded485a34d0f
Shekoo110 <alshekh989@gmail.com>
2026-06-30 21:26:47 +0300

    Update power calculation in clanBattleEngine
    Refactor power calculation to use totalPower and fix return value.

--- clanBattleEngine.js
+++ clanBattleEngine.js
@@ -2,54 +2,45 @@ const useEXAbilities = require("./utils/useEXAbilities")
 
 function calculatePower(player) {
 
-    let power = 0
+    let totalPower = 0
 
     for (const character of player.characters) {
 
-        // قوة الشخصية الأساسية
-        power += character.power || 0
+        let power = character.power || 0
 
-        // قدرات EX
         const ex = useEXAbilities(character)
 
         power += ex.attackBonus * 100
         power += ex.defenseBonus * 80
-
         power += ex.critRate * 60
         power += ex.critDamage * 40
-
         power += ex.dodge * 50
-
         power += ex.shield * 40
-
         power += ex.lifesteal * 50
-
         power += ex.reflect * 50
+
+        totalPower += power
 
     }
 
-    return Math.floor(power)
+    return Math.floor(totalPower)
 
 }
 
 async function clanBattle(playerA, playerB) {
 
     const powerA = calculatePower(playerA)
-
     const powerB = calculatePower(playerB)
-
-    const winner =
-        powerA >= powerB
-            ? playerA.userId
-            : playerB.userId
 
     return {
 
         powerA,
-
         powerB,
 
-        winner
+        winner:
+            powerA >= powerB
+                ? playerA.userId
+                : playerB.userId
 
     }
 
