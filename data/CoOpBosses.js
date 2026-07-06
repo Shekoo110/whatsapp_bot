c4ff2974ebb53efb86d225aa7a5fd2c92a3c9e44
Shekoo110 <alshekh989@gmail.com>
2026-07-06 16:02:25 +0300

    Create CoOpBosses.js

---
+++ data/CoOpBosses.js
@@ -0,0 +1,378 @@
+module.exports = [
+
+{
+    id: "hollow",
+
+    name: "Hollow",
+
+    anime: "Bleach",
+
+    rank: "A",
+
+    image: "",
+
+    hp: 3500000,
+
+    playersLimit: 5,
+
+    rewardType: "a",
+
+    phases: [
+        {
+            hp: 100,
+            skills: [
+                "regeneration"
+            ]
+        }
+    ]
+},
+
+{
+    id: "igris",
+
+    name: "Igris",
+
+    anime: "Solo Leveling",
+
+    rank: "A",
+
+    image: "",
+
+    hp: 3500000,
+
+    playersLimit: 5,
+
+    rewardType: "a",
+
+    phases: [
+        {
+            hp: 100,
+            skills: [
+                "counterSlash"
+            ]
+        }
+    ]
+},
+
+{
+    id: "venom",
+
+    name: "Venom",
+
+    anime: "Marvel",
+
+    rank: "A",
+
+    image: "",
+
+    hp: 3500000,
+
+    playersLimit: 5,
+
+    rewardType: "a",
+
+    phases: [
+        {
+            hp: 100,
+            skills: [
+                "symbioteHeal"
+            ]
+        }
+    ]
+},
+
+// =============================
+// SS
+// =============================
+
+{
+    id: "devil_jin",
+
+    name: "Devil Jin",
+
+    anime: "Tekken",
+
+    rank: "SS",
+
+    image: "",
+
+    hp: 5000000,
+
+    playersLimit: 5,
+
+    rewardType: "ss",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "devilLaser",
+                "darkCombo"
+            ]
+        }
+
+    ]
+},
+
+{
+    id: "darkeater_midir",
+
+    name: "Darkeater Midir",
+
+    anime: "Dark Souls",
+
+    rank: "SS",
+
+    image: "",
+
+    hp: 5000000,
+
+    playersLimit: 5,
+
+    rewardType: "ss",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "darkFlame",
+                "meteor"
+            ]
+        }
+
+    ]
+},
+
+{
+    id: "chaos_max",
+
+    name: "Blue-Eyes Chaos MAX Dragon",
+
+    anime: "Yu-Gi-Oh!",
+
+    rank: "SS",
+
+    image: "",
+
+    hp: 5000000,
+
+    playersLimit: 5,
+
+    rewardType: "ss",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "chaosBreath",
+                "chaosShield"
+            ]
+        }
+
+    ]
+},
+
+{
+    id: "vasto_lorde",
+
+    name: "Vasto Lorde",
+
+    anime: "Bleach",
+
+    rank: "SS",
+
+    image: "",
+
+    hp: 5000000,
+
+    playersLimit: 5,
+
+    rewardType: "ss",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "ceroOscuras",
+                "highRegen"
+            ]
+        }
+
+    ]
+},
+
+// =============================
+// RAID
+// =============================
+
+{
+    id: "placidusax",
+
+    name: "Dragonlord Placidusax",
+
+    anime: "Elden Ring",
+
+    rank: "RAID",
+
+    image: "",
+
+    hp: 8000000,
+
+    playersLimit: 8,
+
+    rewardType: "raid",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "ancientLightning"
+            ]
+        },
+
+        {
+            hp: 70,
+
+            skills: [
+                "bossShield"
+            ]
+        },
+
+        {
+            hp: 40,
+
+            skills: [
+                "massAttack"
+            ]
+        },
+
+        {
+            hp: 15,
+
+            skills: [
+                "rageMode"
+            ]
+        }
+
+    ]
+},
+
+{
+    id: "slifer",
+
+    name: "Slifer the Sky Dragon",
+
+    anime: "Yu-Gi-Oh!",
+
+    rank: "RAID",
+
+    image: "",
+
+    hp: 8000000,
+
+    playersLimit: 8,
+
+    rewardType: "raid",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "thunderStorm"
+            ]
+        },
+
+        {
+            hp: 70,
+
+            skills: [
+                "bossShield"
+            ]
+        },
+
+        {
+            hp: 40,
+
+            skills: [
+                "massAttack"
+            ]
+        },
+
+        {
+            hp: 15,
+
+            skills: [
+                "rageMode"
+            ]
+        }
+
+    ]
+},
+
+{
+    id: "exodia",
+
+    name: "Exodia the Forbidden One",
+
+    anime: "Yu-Gi-Oh!",
+
+    rank: "RAID",
+
+    image: "",
+
+    hp: 8000000,
+
+    playersLimit: 8,
+
+    rewardType: "raid",
+
+    phases: [
+
+        {
+            hp: 100,
+
+            skills: [
+                "sealPower"
+            ]
+        },
+
+        {
+            hp: 70,
+
+            skills: [
+                "bossShield"
+            ]
+        },
+
+        {
+            hp: 40,
+
+            skills: [
+                "massAttack"
+            ]
+        },
+
+        {
+            hp: 15,
+
+            skills: [
+                "rageMode"
+            ]
+        }
+
+    ]
+}
+
+]
