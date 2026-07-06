e69b2067529fa7914ef0df30f90075a0afce6443
Shekoo110 <alshekh989@gmail.com>
2026-07-06 16:17:24 +0300

    Create equipmentBoxes.js

---
+++ data/equipmentBoxes.js
@@ -0,0 +1,86 @@
+module.exports = {
+
+    rareEquipment: {
+
+        name: "📦 Rare Equipment Box",
+
+        rarity: "Rare",
+
+        drops: [
+
+            {
+                rarity: "Rare",
+                chance: 80
+            },
+
+            {
+                rarity: "Epic",
+                chance: 20
+            }
+
+        ]
+
+    },
+
+    epicEquipment: {
+
+        name: "📦 Epic Equipment Box",
+
+        rarity: "Epic",
+
+        drops: [
+
+            {
+                rarity: "Epic",
+                chance: 80
+            },
+
+            {
+                rarity: "Legendary",
+                chance: 20
+            }
+
+        ]
+
+    },
+
+    legendEquipment: {
+
+        name: "👑 Legendary Equipment Box",
+
+        rarity: "Legendary",
+
+        drops: [
+
+            {
+                rarity: "Legendary",
+                chance: 85
+            },
+
+            {
+                rarity: "Mythical",
+                chance: 15
+            }
+
+        ]
+
+    },
+
+    mythicalEquipment: {
+
+        name: "💎 Mythical Equipment Box",
+
+        rarity: "Mythical",
+
+        drops: [
+
+            {
+                rarity: "Mythical",
+                chance: 100
+            }
+
+        ]
+
+    }
+
+}
