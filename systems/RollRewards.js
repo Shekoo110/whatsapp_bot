a10b3a9e702e40d2d7c618a4d97baf276f76fb2a
Shekoo110 <alshekh989@gmail.com>
2026-07-15 22:29:04 +0300

    Create RollRewards.js

---
+++ systems/RollRewards.js
@@ -0,0 +1,22 @@
+const characters = require('../characters.json')
+
+// سنضيفها لاحقاً
+async function createEXReward() {
+
+}
+
+// سنضيفها لاحقاً
+async function createURIIIReward() {
+
+}
+
+// سنضيفها لاحقاً
+async function createURIReward() {
+
+}
+
+module.exports = {
+    createEXReward,
+    createURIIIReward,
+    createURIReward
+}
