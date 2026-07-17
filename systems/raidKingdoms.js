386705900afc62c90392ce76b9551f033dd7ab5f
Shekoo110 <alshekh989@gmail.com>
2026-07-17 03:40:23 +0300

    Refactor string quotes to standard double quotes

--- systems/raidKingdoms.js
+++ systems/raidKingdoms.js
@@ -1,197 +1,287 @@
 module.exports = [
 
 {
-    name: 'وانو',
-    anime: 'One Piece',
-    boss: 'شانكس',
+    name: "وانو",
+    anime: "One Piece",
+    boss: "شانكس",
     hp: 18000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 5,
+    rewardMultiplier: 1.5,
+    passive: "rage",
+    passiveValue: 20,
     abilities: [
-        'هاكي الملك',
-        'ضربة غريفون',
-        'إرهاب الهاكي'
+        "هاكي الملك",
+        "ضربة غريفون",
+        "إرهاب الهاكي"
     ]
 },
 
 {
-    name: 'مارين فورد',
-    anime: 'One Piece',
-    boss: 'ساكازوكي',
+    name: "مارين فورد",
+    anime: "One Piece",
+    boss: "ساكازوكي",
     hp: 18500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 5,
+    rewardMultiplier: 1.5,
+    passive: "burn",
+    passiveValue: 10,
     abilities: [
-        'قبضة الماغما',
-        'حمم بركانية',
-        'عدالة مطلقة'
+        "قبضة الماغما",
+        "حمم بركانية",
+        "عدالة مطلقة"
     ]
 },
 
 {
-    name: 'إلباف',
-    anime: 'One Piece',
-    boss: 'لوكي',
+    name: "إلباف",
+    anime: "One Piece",
+    boss: "لوكي",
     hp: 19500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 12,
+    difficulty: 6,
+    rewardMultiplier: 1.6,
+    passive: "counter",
+    passiveValue: 25,
     abilities: [
-        'غضب العمالقة',
-        'تحطيم الأرض',
-        'صرخة إلباف'
+        "غضب العمالقة",
+        "تحطيم الأرض",
+        "صرخة إلباف"
     ]
 },
 
 {
-    name: 'إمبل داون',
-    anime: 'One Piece',
-    boss: 'ماجلان',
+    name: "إمبل داون",
+    anime: "One Piece",
+    boss: "ماجلان",
     hp: 17500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 10,
+    difficulty: 4,
+    rewardMultiplier: 1.4,
+    passive: "poison",
+    passiveValue: 15,
     abilities: [
-        'سم قاتل',
-        'هيدرا السامة',
-        'ضباب السم'
+        "سم قاتل",
+        "هيدرا السامة",
+        "ضباب السم"
     ]
 },
 
 {
-    name: 'مجتمع الأرواح',
-    anime: 'Bleach',
-    boss: 'ياماموتو',
+    name: "مجتمع الأرواح",
+    anime: "Bleach",
+    boss: "ياماموتو",
     hp: 20000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 20,
+    difficulty: 7,
+    rewardMultiplier: 1.8,
+    passive: "burn",
+    passiveValue: 20,
     abilities: [
-        'لهب الشمس',
-        'زانباكتو النار',
-        'حرق شامل'
+        "لهب الشمس",
+        "زانباكتو النار",
+        "حرق شامل"
     ]
 },
 
 {
-    name: 'هويكو موندو',
-    anime: 'Bleach',
-    boss: 'أولكيورا',
+    name: "هويكو موندو",
+    anime: "Bleach",
+    boss: "أولكيورا",
     hp: 19000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 6,
+    rewardMultiplier: 1.6,
+    passive: "heal",
+    passiveValue: 10,
     abilities: [
-        'لانزا ديل ريلامباغو',
-        'ريسيون',
-        'سيرو أوسكوراس'
+        "لانزا ديل ريلامباغو",
+        "ريسيون",
+        "سيرو أوسكوراس"
     ]
 },
 
 {
-    name: 'قرية الورق',
-    anime: 'Naruto',
-    boss: 'ناروتو',
+    name: "قرية الورق",
+    anime: "Naruto",
+    boss: "ناروتو",
     hp: 19500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 6,
+    rewardMultiplier: 1.7,
+    passive: "rage",
+    passiveValue: 15,
     abilities: [
-        'راسينغان',
-        'قنابل البيجو',
-        'طور الناسك'
+        "راسينغان",
+        "قنابل البيجو",
+        "طور الناسك"
     ]
 },
 
 {
-    name: 'قرية الرمل',
-    anime: 'Naruto',
-    boss: 'غارا',
+    name: "قرية الرمل",
+    anime: "Naruto",
+    boss: "غارا",
     hp: 18000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 10,
+    difficulty: 5,
+    rewardMultiplier: 1.5,
+    passive: "shield",
+    passiveValue: 20,
     abilities: [
-        'درع الرمل',
-        'دفن الرمال',
-        'تسونامي الرمال'
+        "درع الرمل",
+        "دفن الرمال",
+        "تسونامي الرمال"
     ]
 },
 
 {
-    name: 'نازاريك',
-    anime: 'Overlord',
-    boss: 'آينز أوول غاون',
+    name: "نازاريك",
+    anime: "Overlord",
+    boss: "آينز أوول غاون",
     hp: 21000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 20,
+    difficulty: 8,
+    rewardMultiplier: 2,
+    passive: "summon",
+    passiveValue: 1,
     abilities: [
-        'سحر الموت',
-        'الوقت المتوقف',
-        'الاستدعاء الأعظم'
+        "سحر الموت",
+        "الوقت المتوقف",
+        "الاستدعاء الأعظم"
     ]
 },
 
 {
-    name: 'قصر النمل',
-    anime: 'Hunter x Hunter',
-    boss: 'نيفيربيتو',
+    name: "قصر النمل",
+    anime: "Hunter x Hunter",
+    boss: "نيفيربيتو",
     hp: 18500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 6,
+    rewardMultiplier: 1.6,
+    passive: "heal",
+    passiveValue: 15,
     abilities: [
-        'طبيب الدمى',
-        'قفزة الصيد',
-        'هجوم النين'
+        "طبيب الدمى",
+        "قفزة الصيد",
+        "هجوم النين"
     ]
 },
 
 {
-    name: 'برج السماء',
-    anime: 'Hunter x Hunter',
-    boss: 'هيسوكا',
+    name: "برج السماء",
+    anime: "Hunter x Hunter",
+    boss: "هيسوكا",
     hp: 18000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 25,
+    difficulty: 6,
+    rewardMultiplier: 1.6,
+    passive: "counter",
+    passiveValue: 30,
     abilities: [
-        'بانجي غام',
-        'مفاجأة السطح',
-        'بطاقات قاتلة'
+        "بانجي غام",
+        "مفاجأة السطح",
+        "بطاقات قاتلة"
     ]
 },
 
 {
-    name: 'مملكة الألماس',
-    anime: 'Black Clover',
-    boss: 'مارس',
+    name: "مملكة الألماس",
+    anime: "Black Clover",
+    boss: "مارس",
     hp: 18500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 12,
+    difficulty: 5,
+    rewardMultiplier: 1.5,
+    passive: "heal",
+    passiveValue: 10,
     abilities: [
-        'سيف الألماس',
-        'تجديد الجسد',
-        'انفجار الكريستال'
+        "سيف الألماس",
+        "تجديد الجسد",
+        "انفجار الكريستال"
     ]
 },
 
 {
-    name: 'مملكة القلب',
-    anime: 'Black Clover',
-    boss: 'لولوبيتشكا',
+    name: "مملكة القلب",
+    anime: "Black Clover",
+    boss: "لولوبيتشكا",
     hp: 19500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 15,
+    difficulty: 6,
+    rewardMultiplier: 1.7,
+    passive: "shield",
+    passiveValue: 25,
     abilities: [
-        'تنين الماء',
-        'فيضان ملكي',
-        'حاجز الماء'
+        "تنين الماء",
+        "فيضان ملكي",
+        "حاجز الماء"
     ]
 },
 
 {
-    name: 'حلبة القوة',
-    anime: 'Dragon Ball',
-    boss: 'جيرين',
+    name: "حلبة القوة",
+    anime: "Dragon Ball",
+    boss: "جيرين",
     hp: 22000000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 25,
+    difficulty: 10,
+    rewardMultiplier: 2.5,
+    passive: "berserk",
+    passiveValue: 50,
     abilities: [
-        'قبضة العدالة',
-        'طاقة مطلقة',
-        'انفجار القوة'
+        "قبضة العدالة",
+        "طاقة مطلقة",
+        "انفجار القوة"
     ]
 },
 
 {
-    name: 'قلعة الشياطين',
-    anime: 'Solo Leveling',
-    boss: 'باران',
+    name: "قلعة الشياطين",
+    anime: "Solo Leveling",
+    boss: "باران",
     hp: 20500000,
-
+    attack: 15000,
+    defense: 5000,
+    critRate: 18,
+    difficulty: 8,
+    rewardMultiplier: 2,
+    passive: "lightning",
+    passiveValue: 20,
     abilities: [
-        'برق الشيطان',
-        'سيف البرق',
-        'عاصفة الجحيم'
+        "برق الشيطان",
+        "سيف البرق",
+        "عاصفة الجحيم"
     ]
 }
 
