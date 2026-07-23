f46387a24c99feb661564fd0f29dccaa51336c4c
Shekoo110 <alshekh989@gmail.com>
2026-07-23 05:21:19 +0300

    Update Market.js

--- models/Market.js
+++ models/Market.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
 const MarketSchema = new mongoose.Schema({
+
     seller: String,
+
     character: Object,
-    price: Number
+
+    price: Number,
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
 })
 
 module.exports = mongoose.model('Market', MarketSchema)
